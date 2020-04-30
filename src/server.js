58eea0603b8dc990b3491dfaa7290936876efcc2
DiovanePacheti <frontendestudo@gmail.com>
2020-04-30 20:02:55 -0300

    criado deretorios database migrations

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require('express');//importando o modulo
+
+const app = express(); //atribuindo a função express
+
+const routes = require('/.routes'); //importando o arquivo routes 
+
+app.use(express.json());//definindo json nas rotas
+
+app.use(routes);//rotas usadas
+
+app.listen(3333, () => console.log('Server is running'));//ouvindo a porta 3333	
