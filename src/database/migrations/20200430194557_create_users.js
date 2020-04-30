e2ffdf045df0a19fa4f4a996cb4d5a05bb9cfd55
DiovanePacheti <frontendestudo@gmail.com>
2020-04-30 20:16:28 -0300

    aplicando conceito de arrow fuction

--- src/database/migrations/20200430194557_create_users.js
+++ src/database/migrations/20200430194557_create_users.js
@@ -1,13 +1,11 @@
 
-exports.up = function(knex) {
-  return knex.schema.createTable('users', function(table){
+exports.up = knex => knex.schema.createTable('users', function(table){
   	table.increments('id')
   	table.text('username').unique().notNullable()
   	table.timestamp('created_at').defaultTo(knex.fn.now())
   	table.timestamp('updated_at').defaultTo(knex.fn.now())
   });
-};
 
-exports.down = function(knex) {
-  return knex.schema.dropTable('users')
-};
+
+exports.down = knex => knex.schema.dropTable('users');
+
