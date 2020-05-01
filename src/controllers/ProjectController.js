58c577045bbc9a5ce6b3da97e49728f655300f52
DiovanePacheti <frontendestudo@gmail.com>
2020-05-01 19:04:17 -0300

    create paginação

--- src/controllers/ProjectController.js
+++ src/controllers/ProjectController.js
@@ -4,10 +4,12 @@ module.exports = {
 
 	async index(request, response, next){
 		try{
+			//page recebe 1 como valor padrao caso nao exista este parametro
+			const {user_id, page = 1} = request.query;//recebe por parametro nomeado 
 
-			const {user_id} = request.query;//recebe por parametro nomeado 
+			const query = knex('projects').limit(5).offset((page - 1) * 5)
 
-			const query = knex('projects')
+			const countObj = knex('projects').count();//capturando o total de registros
 
 			if(user_id){//se o user_if true
 				query
@@ -15,7 +17,14 @@ module.exports = {
 					//trabalha com a tabela users quando verificado os ids das tabelas
 					.join('users', 'users.id', '=', 'projects.user_id')
 					.select(['projects.*','users.username'])
+
+				//calcular o total de requitro do id referente	
+				countObj
+					.where({user_id})
 			}
+
+			const [count] = await countObj;
+			response.header('X-Total_Count', count['count(*)']);
 
 			const results = await query;
 
